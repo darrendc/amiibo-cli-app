781e2bca7d113bf4da8acf460988a18a267a514a
darrendc <ddcosta@gmail.com>
2019-04-10 16:27:42 -0400

    added CLI loop to consider blank space input

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,21 +5,27 @@ class CLI
     puts "Welcome to my Nintendo Amiibo application!"
     puts "Amiibo's are Nintendo themed figurines that utilize near field communication (NFC) and radio frequency identification (RFID) to interact with Nintendo game consoles and provide in-game content"
     puts "With over 150 different amiibos in existense, the purpose of this application is to provide a detailed description of a specific amiibo you select"
-
+    puts "Type 'exit' to leave the application"
+    puts " "
     puts "First, input a letter to generate a list of amiibos that begin with that letter:"
     letter = gets.chomp.downcase
     while letter != "exit"
-      if letter.length > 1
-        puts "Too many characters! Please input a single letter:"
+      if letter =~ /\A\s*\Z/
+        puts "No blank spaces! Please input a single letter:"
         letter = gets.chomp.downcase
-      elsif !(letter =~ /[a-zA-Z]/)
+      elsif letter.length > 1
+        puts "Too many characters or invalid format! Please input a single letter:"
+        letter = gets.chomp.downcase
+      elsif !(letter =~ /[a-z]/)
         puts "Please select a letter in english from A-Z:"
         letter = gets.chomp.downcase
       else
-        puts "works" #would generated a scraped list of amiibos starting with user input 'letter'
-        break
+        puts letter #display generated list of amiibos starting with user input 'letter'
+      break
       end
     end
+
+
   end
   #Scrape and save all amiibos
 end
